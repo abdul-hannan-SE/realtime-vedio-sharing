73de797661de3a8e461056c4bd1393f5682e2b93
Abdul Hannan <114971771+abdul-hannan-SE@users.noreply.github.com>
2024-09-21 12:49:45 +0500

    enhanced application structure

---
+++ utils/asyncHandler.js
@@ -0,0 +1,23 @@
+// high level function that returns a function 
+const { clearFile } = require("./common");
+const asyncHandler=(fn)=> async (req,res,next)=>{
+    try{
+      return await fn(req,res,next);
+    }catch(err){
+        if (req.file) {
+            clearFile(
+              path.join(
+                __dirname,
+                "..",
+                "resources",
+                "images",
+                "profile_pics",
+                req.file.filename
+              )
+            );
+          }
+        next(err);
+    }
+}
+
+module.exports={asyncHandler};
